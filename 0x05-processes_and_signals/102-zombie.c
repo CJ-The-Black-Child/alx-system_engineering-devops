a9b34adc134251428baa348376a1cbb29d1c0114
Congo Justus Migue <congomigue@gmail.com>
2023-06-30 18:52:36 +0000

    Fixed syntax for printf

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -34,7 +34,7 @@ int main(void)
 		child_pid = fork();
 		if (child_pid == 0)
 			return (0);
-		printf("Zombie process created, PID: %d\n," child_pid);
+		printf("Zombie process created, PID: %d\n", child_pid);
 	}
 
 	infinite_while();
