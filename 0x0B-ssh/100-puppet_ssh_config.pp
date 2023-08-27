7c79eeeb72d5c59c757b6161df4415ff7adf73b9
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-27 23:50:11 +0000

    Update to Advanced puppet task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 # To ensure that SSH client config file exists
 include stdlib
 
-file_line {'Turn off passwd auth':
-ensure => present,
-path => '/etc/ssh/ssh_config',
-line => 'PasswordAuthentication no',
-replace => true,
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
+
 file_line { 'Declare identity file':
-ensure => present,
-path => '	IdentityFIle ~/.ssh/school',
-replace => true,
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
