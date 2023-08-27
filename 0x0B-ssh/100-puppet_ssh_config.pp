9031ab9e820f99f895119cfbe6301bdb54d3a287
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-27 22:50:27 +0000

    Advanced Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# To ensure that SSH client config file exists
+include stdlib
+
+file_line {'Turn off passwd auth':
+ensure => present,
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no',
+replace => true,
+}
+file_line { 'Declare identity file':
+ensure => present,
+path => '	IdentityFIle ~/.ssh/school',
+replace => true,
+}
