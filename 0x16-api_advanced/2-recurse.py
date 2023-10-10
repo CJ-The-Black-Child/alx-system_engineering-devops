1f614ec2c6512d13fd202d271d3995cfc5066807
Congo Justus Migue <congomigue@gmail.com>
2023-10-10 16:43:55 +0300

    meaningful and more concise changes to 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ containing the title of all hot articles for a given subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """
     This returns a list of containing titles of all hot
     articles on a subreddit.
@@ -15,8 +15,10 @@ def recurse(subreddit, hot_list=[]):
     headers = {
         "User-Agent": "linux: 0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {"limit": 100}
-
+    params = {"after": after,
+              "count": count,
+              "limit": 100
+              }
     response = requests.get(
         url,
         headers=headers,
@@ -31,12 +33,15 @@ def recurse(subreddit, hot_list=[]):
         return None
 
     data = response.json().get("data")
-    hot_list.extend(post["data"]["title"] for post in data["children"])
+    after = data.get("after")
+    count += data.get("dist")
+    for i in data.get("children"):
+        hot_list.append(i.get("data").get("title"))
 
     """
     if there are more pages, recursively call the function
     """
-    if data["after"] is not None:
-        return recurse(subreddit, hot_list)
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
 
     return hot_list
