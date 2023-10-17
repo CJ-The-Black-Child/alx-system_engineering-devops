ee6ee11ea673dd20197293f5b46aa4e5df35bb05
Congo Justus Migue <congomigue@gmail.com>
2023-10-17 17:14:28 +0300

    debugging our web app

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This increases the ammount of traffic on Nginx server
+# increment the Limit of the default file
+exec { 'fix--got-nginx':
+command => 'sed -i "/15/4096" /etc/default/nginx',
+path    => '/usr/local/bin/:/bin/'
+} ->
+
+# To restart Nginx
+exec { 'nginx-restart':
+command => 'nginx restart',
+path    => '/etc/init.d/'
+}
