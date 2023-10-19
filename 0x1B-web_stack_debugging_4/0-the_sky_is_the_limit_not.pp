8bd5c3009d306f7ffd87c55aa6fc75133b760c6c
CJ-The-Black-Child <congomigue@gmail.com>
2023-10-19 22:18:39 +0000

    Updated bug in 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # This increases the ammount of traffic on Nginx server
 # increment the Limit of the default file
 exec { 'fix--got-nginx':
-command => 'sed -i "/15/4096" /etc/default/nginx',
-path    => '/usr/local/bin/:/bin/'
-} ->
+  command => 'sed -i "/15/4096" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
 
 # To restart Nginx
 exec { 'nginx-restart':
-command => 'nginx restart',
-path    => '/etc/init.d/'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
